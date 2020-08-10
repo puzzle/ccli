b493a76a86cf89a4f4df4dc96b75b4d7f4954e90
Nils Rauch <rauch@puzzle.ch>
2020-08-10 10:52:29 +0200

    Add login command, refs: #54910

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,17 +2,34 @@
 
 require 'rubygems'
 require 'commander'
+require 'tty-exit'
+
+require_relative './adapters/session_adapter.rb'
 
 class CLI
   include Commander::Methods
 
+  # rubocop:disable Metrics/MethodLength
   def run
     program :name, 'ccli - cryptopus ccli'
     program :version, '0.1.0'
     program :description, 'CLI tool to manage openshift secrets'
 
+    command :login do |c|
+      c.syntax = 'ccli login <url> [options]'
+      c.description = 'ccli login'
+      c.option '--token TOKEN', String, 'Authentification Token including api user username'
+
+      c.action do |args, options|
+        TTY::Exit.exit_with(:usage_error, :default) if args.empty?
+        SessionAdapter.instance.update_session(options.token, args.first)
+        puts 'Successfully logged in'
+      end
+    end
+
     run!
   end
+  # rubocop:enable Metrics/MethodLength
 end
 
 CLI.new.run if $PROGRAM_NAME == __FILE__
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-
 RSpec.configure do |rspec|
 
 end
